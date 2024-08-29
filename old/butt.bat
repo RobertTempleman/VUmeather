fe1a1b8ee0d34512d62a90b8d845fc63308fb881
bert.bert <rob65536>
2024-08-29 01:00:01 +0100

    Berty II

---
+++ old/butt.bat
@@ -0,0 +1,6 @@
+fart -r sw\* geiger metald
+fart -r sw\* GEIGER METALD
+fart -r -B -b sw\* geiger metald
+fart -r -B -b sw\* GEIGER METALD
+fart -r -f sw\* geiger metald
+fart -r -f sw\* GEIGER METALD
---
+++ old/butt_nobin.bat
@@ -0,0 +1,4 @@
+fart -r sw\* urei545 metaldt
+fart -r sw\* UREI545 METALDT
+fart -r -f sw\* urei545 metaldt
+fart -r -f sw\* UREI545 METALDT
